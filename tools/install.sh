8270d6002a4e24feb16ff987b1562b2c08d4379b
Garkusha Andrey <agarkush@mera.ru>
2016-05-04 19:05:15 +0300

    tools: compile and install
    Just improve old tool. Changed API and now it can compile

--- tools/install.sh
+++ tools/install.sh
@@ -10,25 +10,38 @@ MODULES[2]='dia/ebin'
 
 function usage() {
 cat <<HELP
-I'll just copy all *beam and *.sh to one directory
- 
+Simple tool to compile all applications and install them
+
 Use me like this:
-./install.sh ~/git/diameter-relay-agent my_path/
-
-
+./install.sh ~/git/diameter-relay-agent
 HELP
+exit
+}
+
+function compile_apps() {
+    cd ${diameter_dir}/controller
+    rebar compile
+
+    cd ${diameter_dir}/dia
+    rebar compile
 }
 
 function copy_diameter_apps_to_dir() {
-    diameter_dir=$1
-    to_dir=$2
+    local to_dir="$diameter_dir/Result"
     echo "Diameter location: ${diameter_dir}"
     echo "Destination directory: ${to_dir}"
+
+    [ -d $to_dir ] || mkdir $to_dir
 
     `cp ${diameter_dir}/boot/*.sh               ${to_dir}`
     `cp ${diameter_dir}/controller/ebin/*.beam  ${to_dir}`
     `cp ${diameter_dir}/dia/ebin/*.beam         ${to_dir}`
 }
 
-usage
-copy_diameter_apps_to_dir $1 $2
+diameter_dir=$1
+if [ ! -d "$diameter_dir" ]; then
+    usage
+fi
+
+compile_apps
+copy_diameter_apps_to_dir
