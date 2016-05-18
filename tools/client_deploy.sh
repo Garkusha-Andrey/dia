dca8ee8f7c5b6825a044259c71dec0d2c390cdee
Garkusha Andrey <agarkush@mera.ru>
2016-05-18 20:21:56 +0300

    tools: clients start script
    Add start script for remote clients. Usage of the script:
    ./client_deploy.sh <dir with client.beam> <config file>
    config file should be like diameter-relay-agent/configs/clients.conf

---
+++ tools/client_deploy.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "deploy servers"
+
+function usage() {
+cat <<HELP
+Use me like this:
+./client_deploy.sh ~/git/diameter-relay-agent/Result clients.conf
+HELP
+exit
+}
+
+function start {
+  servers_config=$1
+  while read -r real_line; do
+
+    if [[ $real_line != "{"* ]]; then
+        continue
+    fi
+
+    line=$(echo $real_line | tr -d '{' | tr -d ' ' | tr -d '}')
+
+    HostID=$(echo $line | cut -d "," -f 1 | tr -d '"')
+    RealmID=$(echo $line | cut -d "," -f 2 | tr -d '"')
+    LIP=$(echo $line | cut -d "," -f 3)
+    RIP=$(echo $line | cut -d "," -f 4)
+    Port=$(echo $line | cut -d "," -f 5)
+    NodeName=$(echo $HostID | cut -d "." -f 1)
+
+    echo "Realm: $RealmID, Host: $HostID, local IP: $LIP, server IP: $RIP Port: $Port"
+    local local_deploy="-s client deploy '$NodeName' '$RealmID' $LIP $RIP $Port"
+    ERL_COMM="$ERL_COMM $local_deploy"
+
+  done < "$servers_config"
+
+  START_COMM="erl -sname cli $ERL_COMM"
+  eval $START_COMM
+}
+
+# main
+bin_dir=$1
+srv_bin=$1/client.beam
+config_file=$2
+if [ ! -f "$srv_bin" ] || [ ! -f $config_file ]; then
+    if [ ! -f "$srv_bin" ]; then
+        echo "didn't find client.beam in $srv_bin"
+    fi
+    if [ ! -f $config_file ]; then 
+        echo "didn't find config file"
+    fi
+    usage
+fi
+
+cd $bin_dir
+start $config_file
+
