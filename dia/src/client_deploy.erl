37f02fc39af0dbc8c21577fde5d0edd615c91754
Garkusha Andrey <agarkush@mera.ru>
2016-05-24 16:16:46 +0300

    diameter: API to start client

--- dia/src/client_deploy.erl
+++ dia/src/client_deploy.erl
@@ -1,22 +1,43 @@
 -module(client_deploy).
 
--export([start/0,
-		 start_traffic/0]).
+-export([start/2,
+		 start_traffic/0,
+		 add_clients/4]).
 
-start() ->
-	io:format("Start of Erlang client_deploy~n").
+-define(RIP, remote_ip).
+%%-define(LOCAL_IP, '127.0.0.1').
+%%-define(REMOTE_IP, '127.0.0.1').
+-define(SERVER_PORT, '3911').
+-define(TRAFFIC_INTERVAL, 2000).
+
+start(LocalIP, RemoteIp) ->
+	io:format("Start of Erlang client_deploy~n"),
+	add_clients('c', LocalIP, RemoteIp, 5).
+
+add_clients(Pattern, LocalIP, RemoteIp, Count) ->
+	io:format("Count: ~p~n", [Count]),
+	add_clients_pattern(Pattern, 'ex.com', LocalIP, RemoteIp, ?SERVER_PORT, 0, Count).
 
 start_traffic() ->
 	continue_call_stub().
 
 continue_call_stub() ->
-	continues_call(c1, "s1", "ex1.com", 2000),
-	continues_call(c1, "s2", "ex2.com", 2001),
-	continues_call(c1, "s3", "ex3.com", 2002),
-	continues_call(c1, "s4", "ex4.com", 2003),
-	continues_call(c1, "s5", "ex5.com", 2004).
+	continues_call(c_1, "s1", "ex1.com", ?TRAFFIC_INTERVAL),
+	continues_call(c_2, "s2", "ex2.com", ?TRAFFIC_INTERVAL),
+	continues_call(c_3, "s3", "ex3.com", ?TRAFFIC_INTERVAL),
+	continues_call(c_4, "s4", "ex4.com", ?TRAFFIC_INTERVAL).
 
 continues_call(Name, Realm, Host, IntervalMS) ->
 	client:call(Name, user, Realm, Host),
 	timer:sleep(IntervalMS)
 	.
+
+%% 'c1','ex.ru','127.0.0.1','127.0.0.1','3911'
+add_clients_pattern(NamePattern, Realm, LocalIP, RemoteIP, Port, To, To) ->
+	ServiceName = list_to_atom(lists:concat([NamePattern, "_", To])),
+	client:deploy([ServiceName, Realm, LocalIP, RemoteIP, Port]);
+
+add_clients_pattern(NamePattern, Realm, LocalIP, RemoteIP, Port, From, To) ->
+	ServiceName = list_to_atom(lists:concat([NamePattern, "_", From])),
+	client:deploy([ServiceName, Realm, LocalIP, RemoteIP, Port]),
+	add_clients_pattern(NamePattern, Realm, LocalIP, RemoteIP, Port, From+1, To).
